0faeb30c02f880ee1253d1e9dce224b5a6cd5825
rodpedrassi <rodpedrassi@gmail.com>
2022-11-11 17:19:12 -0300

    configuração do .env mas sem sucesso

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,9 +3,12 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 3007,
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  // user: 'root',
+  // password: 'password',
+  // database: 'StoreManager',
 });
 
 module.exports = connection;
